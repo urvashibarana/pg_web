7897c3d176d5859cc10ab18705fe6ee7a3dd52bc
urvashibarana <ranaurvashiba21@gmail.com>
2025-08-03 16:44:31 +0530

    Update page.tsx

--- app/pg/[id]/page.tsx
+++ app/pg/[id]/page.tsx
@@ -1,22 +1,9 @@
 import PGDetail from './PGDetail';
-import { Metadata } from 'next';
 
-export async function generateStaticParams(): Promise<{ id: string }[]> {
+export async function generateStaticParams() {
   return ['1', '2', '3', '4', '5', '6'].map(id => ({ id }));
 }
 
-type Props = {
-  params: {
-    id: string;
-  };
-};
-
-export async function generateMetadata({ params }: Props): Promise<Metadata> {
-  return {
-    title: `PG Listing ${params.id}`,
-  };
-}
-
-export default function PGPage({ params }: Props) {
+export default function Page({ params }: { params: { id: string } }) {
   return <PGDetail pgId={params.id} />;
 }
